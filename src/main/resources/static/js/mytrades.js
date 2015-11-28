3a966c4ea4bd4e3d4af622a94ca663cd304f1f30
Shaun Cooper <shaun.11.cooper@gmail.com>
2015-11-28 16:16:29 -0500

    Message de confirmation du delete dun trade

--- src/main/resources/static/js/mytrades.js
+++ src/main/resources/static/js/mytrades.js
@@ -12,8 +12,35 @@ $(window).resize(function(){
 
     }
 });
+var deleteTradeID;
+function showDeleteConfirmation(tradeID){
+    document.querySelector('#openModalDelete').classList.add('modal-box-show');
+    deleteTradeID = tradeID;
+}
+function modalClose() {
+    $(".modal-box, .modal-overlay").fadeOut(500, function () {
+        $(".modal-box, .modal-overlay").removeClass("modal-box-show");
+    });
+}
 
-function deleteTrade(tradeID){
-    document.getElementById("tradeID").value = tradeID;
+function deleteTrade(){
+    alert(deleteTradeID);
+    document.getElementById("tradeID").value = deleteTradeID;
     document.getElementById("deleteTradeForm").submit();
+}
+$(init);
+function init(){
+    $(".js-modal-close, .modalDialogDelete").click(function () {
+        modalClose();
+    });
+    // Handle ESC key (key code 27)
+    document.addEventListener('keyup', function (e) {
+        if (e.keyCode == 27) {
+            modalClose();
+        }
+    });
+    //Prevent le close si on click sur le modal lui-meme
+    document.getElementById("openModalDelete").children[0].addEventListener('click', function (e) {
+        e.stopPropagation();
+    }, false);
 }
