050e9e82d97e3c4c6375af27be4339921d99581e
Manu <ebeloin@hotmail.com>
2015-11-28 02:43:33 -0500

    Pagination terminée pour toutes les pages

--- src/main/java/com/tilf/troke/controller/SearchController.java
+++ src/main/java/com/tilf/troke/controller/SearchController.java
@@ -49,7 +49,7 @@ public class SearchController {
 
     // Test de catégorie
     @RequestMapping(value = "/category", method = RequestMethod.GET)
-    public String ListCategoryItems(@RequestParam("categoryName") String categoryName, @RequestParam(value = "catIsChecked") Boolean catIsChecked, Model model, HttpSession session) {
+    public String ListCategoryItems(@RequestParam("categoryName") String categoryName, @RequestParam(value = "catIsChecked") Boolean catIsChecked, @RequestParam(value = "page", required = false) String page, @RequestParam(value = "number", required = false) Integer number, Model model, HttpSession session) {
         List<CustomSearchObjectEntity> list = new ArrayList<>();
         // Modification de la liste de Cat/SubCat
         if (catIsChecked == true) {
@@ -76,12 +76,90 @@ public class SearchController {
         if (!catSubCats.isEmpty()) {
             // Transformer la liste en liste de custom object entity
             list = customObjectRepository.getObjectsBySubCategory(catSubCatService.getSubCatFromSet(catSubCats));
-            model.addAttribute("objectList", list);
         } else {
-            model.addAttribute("objectList", null);
+            list = null;
             model.addAttribute("objectListEmpty", true);
         }
 
+        // Début
+
+        List<List<CustomSearchObjectEntity>> pageList = new ArrayList<>();
+        Integer pageNumber = null;
+
+        // Si number est nul, on lui met 10 par défaut
+        if (number == null) {
+            number = 10;
+        }
+
+        // Nombre total de pages de résultats
+        Double numberDouble = Double.parseDouble(number.toString());
+        int totalPageNumber = (int) Math.ceil((list.size() / numberDouble));
+
+        // On vérifie si suivant ou précédent est reçu
+        // Notons que la première page est à 0
+        if (page != null) {
+            if (!isInteger(page)) {
+                if (page.contains("plus")) {// Suivant
+                    pageNumber = Integer.parseInt(page.replace("plus", "").trim()); // On enlève le char qui indentifiait la direction
+                    if (pageNumber < totalPageNumber) {
+                        pageNumber++;
+                    } else {
+                        pageNumber = totalPageNumber;
+                    }
+                } else if (page.contains("minus")) { // Précédent
+                    pageNumber = Integer.parseInt(page.replace("minus", "").trim()); // On enlève le char qui indentifiait la direction
+                    if (pageNumber > 1) {
+                        pageNumber--;
+                    } else {
+                        pageNumber = 1; // la première page dans le tableau
+                    }
+                }
+            } else {
+                pageNumber = Integer.parseInt(page);
+                if (pageNumber < 0) {
+                    pageNumber = 1;
+                } else if (pageNumber > totalPageNumber) {
+                    pageNumber = totalPageNumber;
+                }
+            }
+        } else { // Si page est nulle, on lui affecte 1 pour la première page
+            pageNumber = 1;
+        }
+
+        // Création des pages (liste de liste de customsearchobjectentity)
+        if (list.size() > number) {
+            for (int i = 0; i < totalPageNumber; i++) {
+                List<CustomSearchObjectEntity> internalList = new ArrayList<>();
+                if (i < totalPageNumber - 1) {
+                    for (int j = 0; j < number; j++) {
+                        if (i == 0) {
+                            internalList.add(list.get(j));
+                        } else {
+                            internalList.add(list.get((i * number) + j));
+                        }
+                    }
+                    pageList.add(internalList);
+                } else {
+                    for (int j = 0; j < list.size() % number; j++) {
+                        internalList.add(list.get((i * number) + j));
+                    }
+                    pageList.add(internalList);
+                }
+            }
+            model.addAttribute("objectList", pageList.get(pageNumber - 1)); // On transforme pageNumber en index
+        } else {
+            model.addAttribute("objectList", list);
+        }
+
+        // Attribut de modèle pour les pages
+        model.addAttribute("currentpage", pageNumber); // On transforme ici l'index du tableau en page
+        model.addAttribute("numberperpage", number);
+        model.addAttribute("numberofpage", totalPageNumber);
+        model.addAttribute("totalobjetnumber", list);
+
+        // fin
+
+        model.addAttribute("adrGetRequest", "/category?categoryName=" + categoryName + "&amp;catIsChecked=" + catIsChecked);
         model.addAttribute("adrStartTrade", "/startTrade?itemID=");
         model.addAttribute("leftMenu", fillLeftCatMenu());
 
@@ -97,7 +175,7 @@ public class SearchController {
     }
 
     @RequestMapping(value = "/subcategory", method = RequestMethod.GET)
-    public String ListSubCategoryItems(@RequestParam("subCategoryName") String subCategoryName, @RequestParam(value = "subCatIsChecked") Boolean subCatIsChecked, HttpSession session, Model model) {
+    public String ListSubCategoryItems(@RequestParam("subCategoryName") String subCategoryName, @RequestParam(value = "subCatIsChecked") Boolean subCatIsChecked, @RequestParam(value = "page", required = false) String page, @RequestParam(value = "number", required = false) Integer number, HttpSession session, Model model) {
         List<CustomSearchObjectEntity> list = new ArrayList<>();
         // Modification de la liste de Cat/SubCat
         if (subCatIsChecked == true) {
@@ -111,22 +189,94 @@ public class SearchController {
         if (!catSubCats.isEmpty() && !catSubCatService.getSubCatFromSet(catSubCats).isEmpty()) {
             if (!catSubCatService.getSubCatFromSet(catSubCats).isEmpty()) {
                 list = customObjectRepository.getObjectsBySubCategory(catSubCatService.getSubCatFromSet(catSubCats));
-                model.addAttribute("objectList", list);
+
 
             } else {
-                model.addAttribute("objectList", "");
+                list = null;
             }
         } else {
-            model.addAttribute("objectList", null);
+            list = null;
             model.addAttribute("objectListEmpty", true);
         }
 
         // debut
 
+        List<List<CustomSearchObjectEntity>> pageList = new ArrayList<>();
+        Integer pageNumber = null;
 
+        // Si number est nul, on lui met 10 par défaut
+        if (number == null) {
+            number = 10;
+        }
+
+        // Nombre total de pages de résultats
+        Double numberDouble = Double.parseDouble(number.toString());
+        int totalPageNumber = (int) Math.ceil((list.size() / numberDouble));
+
+        // On vérifie si suivant ou précédent est reçu
+        // Notons que la première page est à 0
+        if (page != null) {
+            if (!isInteger(page)) {
+                if (page.contains("plus")) {// Suivant
+                    pageNumber = Integer.parseInt(page.replace("plus", "").trim()); // On enlève le char qui indentifiait la direction
+                    if (pageNumber < totalPageNumber) {
+                        pageNumber++;
+                    } else {
+                        pageNumber = totalPageNumber;
+                    }
+                } else if (page.contains("minus")) { // Précédent
+                    pageNumber = Integer.parseInt(page.replace("minus", "").trim()); // On enlève le char qui indentifiait la direction
+                    if (pageNumber > 1) {
+                        pageNumber--;
+                    } else {
+                        pageNumber = 1; // la première page dans le tableau
+                    }
+                }
+            } else {
+                pageNumber = Integer.parseInt(page);
+                if (pageNumber < 0) {
+                    pageNumber = 1;
+                } else if (pageNumber > totalPageNumber) {
+                    pageNumber = totalPageNumber;
+                }
+            }
+        } else { // Si page est nulle, on lui affecte 0 pour la première page
+            pageNumber = 1;
+        }
+
+        // Création des pages (liste de liste de customsearchobjectentity)
+        if (list.size() > number) {
+            for (int i = 0; i < totalPageNumber; i++) {
+                List<CustomSearchObjectEntity> internalList = new ArrayList<>();
+                if (i < totalPageNumber - 1) {
+                    for (int j = 0; j < number; j++) {
+                        if (i == 0) {
+                            internalList.add(list.get(j));
+                        } else {
+                            internalList.add(list.get((i * number) + j));
+                        }
+                    }
+                    pageList.add(internalList);
+                } else {
+                    for (int j = 0; j < list.size() % number; j++) {
+                        internalList.add(list.get((i * number) + j));
+                    }
+                    pageList.add(internalList);
+                }
+            }
+            model.addAttribute("objectList", pageList.get(pageNumber - 1)); // On transforme pageNumber en index
+        } else {
+            model.addAttribute("objectList", list);
+        }
+
+        // Attribut de modèle pour les pages
+        model.addAttribute("currentpage", pageNumber); // On transforme ici l'index du tableau en page
+        model.addAttribute("numberperpage", number);
+        model.addAttribute("numberofpage", totalPageNumber);
+        model.addAttribute("totalobjetnumber", list);
 
         // fin
-
+        model.addAttribute("adrGetRequest", "/subcategory?subCategoryName=" + subCategoryName + "&amp;subCatIsChecked=" + subCatIsChecked);
         model.addAttribute("leftMenu", fillLeftCatMenu());
         model.addAttribute("adrStartTrade", "/startTrade?itemID=");
 
@@ -242,6 +392,7 @@ public class SearchController {
         model.addAttribute("numberperpage", number);
         model.addAttribute("numberofpage", totalPageNumber);
         model.addAttribute("keyword", keyword);
+        model.addAttribute("totalobjetnumber", list);
 
         // Autres attributs de modèle
         model.addAttribute("adrSearch", "/searchDB?keyword=");
