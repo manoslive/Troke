fa98b516c26470e2f3f2e5dfd9f52cf4b143d9fb
Godfatheralex21 <buba_dude_16@hotmail.com>
2015-11-29 13:53:02 -0500

    Cookie au login ..

--- src/main/java/com/tilf/troke/controller/LoginController.java
+++ src/main/java/com/tilf/troke/controller/LoginController.java
@@ -6,11 +6,15 @@ import com.tilf.troke.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.context.WebContext;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -36,11 +40,22 @@ public class LoginController {
     }
 
     @RequestMapping("/logout")
-    public String logout(HttpSession session) {
+    public String logout(HttpSession session, HttpServletRequest req,HttpServletResponse resp) {
         authContext.getUser().setIsonline("N");
         userRepository.save(authContext.getUser());
         authContext.setUser(null);
         session.removeAttribute("user");
+
+        // on retire toutes les cookies utilisé pour facilité l'utilisation du user ..
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null)
+            for (int i = 0; i < cookies.length; i++) {
+                cookies[i].setValue("");
+                cookies[i].setPath("/");
+                cookies[i].setMaxAge(0);
+                resp.addCookie(cookies[i]);
+            }
+
 
         // TODO THYMELEAF HACK
         if (false) {
@@ -63,16 +78,33 @@ public class LoginController {
     }
 
     @RequestMapping("/openModalConnexion")
-    public String openModalConnexion(HttpSession session)
+    public String openModalConnexion(HttpSession session,
+                                     @CookieValue(value="Connect", defaultValue = "empty") String userCookie)
     {
         session.removeAttribute("error");
+        if(!userCookie.equals("empty"))
+        {
+            UsersEntity user = userRepository.findUsersEntityByIduser(userCookie);
+            session.setAttribute("user", user);
+            authContext.setUser(user);
+            return "redirect:/";
+        }
         return "redirect:#openModalConnexion"; // FIXME la page refresh au moment du click
     }
 
     @RequestMapping(value="/auth", method= RequestMethod.POST)
-    public String login(@RequestParam("iduser") String idUser, @RequestParam("pass") String pass, Model model, HttpSession session) {
+    public String login(@RequestParam("iduser") String idUser,
+                        @RequestParam("pass") String pass,
+                        @RequestParam(value = "StayConnected", defaultValue = "notStay") String stay,
+                        Model model,
+                        HttpSession session, HttpServletResponse response) {
         UsersEntity user = userRepository.findUsersEntityByIduserAndPass(idUser, pass);
-        session.setAttribute("user", user);
+        //session.setAttribute("user", user);
+
+        // si l'utilisateur coche il restera logué a son retour ..
+        if(stay.equals("Stay")) {
+            response.addCookie(new Cookie("Connect", user.getIduser()));
+        }
 
         if(idUser.isEmpty())
         {
@@ -109,7 +141,7 @@ public class LoginController {
 
         else
         {
-            userRepository.save(user);
+            //userRepository.save(user);
             authContext.setUser(user);
             session.setAttribute("user", user);
 
